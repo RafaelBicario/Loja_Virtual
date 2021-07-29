a510c71a1280221f4ce9de863ec58d2a44d2f9f9
RafaelBicario <rafabicario@outlook.com>
2021-07-29 16:12:00 -0300

    commit restante aula 11

---
+++ src/main/java/com/dev/loja/repositorios/FuncionarioRepositorio.java
@@ -0,0 +1,7 @@
+package com.dev.loja.repositorios;
+
+import com.dev.loja.modelos.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepositorio extends JpaRepository<Funcionario, Long> {
+}
