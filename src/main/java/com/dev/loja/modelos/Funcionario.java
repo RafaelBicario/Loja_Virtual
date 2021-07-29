05dfa94f9f2d64ad1334ed8f89f587ea494d3446
RafaelBicario <rafabicario@outlook.com>
2021-07-29 16:08:11 -0300

    Commit aula 11

--- src/main/java/com/dev/loja/modelos/Funcionario.java
+++ src/main/java/com/dev/loja/modelos/Funcionario.java
@@ -33,13 +33,13 @@ public class Funcionario implements Serializable {
     private String uf;
     private String cep;
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
